5c3892d90c6c790acb168298ff21e841d1987c13
Ibnu Abdul Azis <ibnuabdulazis@Ibnus-MacBook-Pro.local>
2017-07-26 03:28:02 +0700

    try to fix view composer 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Auth;
+Use App\Notification;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,7 +19,16 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         \Carbon\Carbon::setlocale('id');
-        View::composer('*', 'App\Http\ViewComposers\HeaderComposer');
+        // View::composer('*', 'App\Http\ViewComposers\HeaderComposer');
+        View::composer('*', function($view){
+            if (Auth::check()) {
+                $notifcountunreadable = Notification::where('user_id', Auth::user()->id)->where('read', '1')->count();
+                $notification = Notification::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+                $view->with('notif', $notification);
+                $view->with('notifcount', $notifcountunreadable);
+
+            }
+        });
     }
 
     /**
