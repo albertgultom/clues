65287d4c238afe0628e56102a99a464a69aae60e
Ibnu Abdul Azis <ibnuabdulazis@Ibnus-MacBook-Pro.local>
2017-07-26 02:28:20 +0700

    tes push1

--- app/Http/ViewComposers/HeaderComposer.php
+++ app/Http/ViewComposers/HeaderComposer.php
@@ -17,7 +17,7 @@ class HeaderComposer
 */
 public function compose (View $view)
 {
-	if (Auth::check()) {
+	if (Auth::check () ) {
 		$id = Auth::user()->id;
 		$notifcountunreadable = Notification::where('user_id', $id)->where('read', '1')->count();
 		$notification = Notification::where('user_id', $id)->orderBy('created_at', 'desc')->get();
